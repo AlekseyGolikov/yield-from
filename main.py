726f95a9dd66a2db722e8b786d3ffcadb8d7aeea
AlekseyGolikov <golikov.aleksey.1987@yandex.ru>
2022-06-04 09:58:32 +0300

    add main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+
+def gen1():
+    yield 1
+    yield 2
+    yield 3
+    yield 4
+
+def gen2():
+    yield 9000
+
+    for i in gen1():
+        yield i
+
+    yield 9999
+
+for i in gen2():
+    print(i)
